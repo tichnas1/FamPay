a59758bf852ac0c2356946ce552af3dcc4851142
tichnas <sanchit.arora.2002@gmail.com>
2022-08-20 18:53:53 +0530

    Implement formatted text (except font style)

--- src/components/FormatText/index.js
+++ src/components/FormatText/index.js
@@ -1,10 +1,29 @@
+import { Fragment } from 'react';
+
 function FormatText({ formatData, fallback, tag, className }) {
   if (!formatData && !fallback) return null;
 
   const CustomTag = tag || 'p';
 
-  // TODO: Implement formatted string
-  return <CustomTag className={className}>{fallback}</CustomTag>;
+  const { text, entities } = formatData;
+  const textPieces = text.split('{}');
+
+  if (textPieces.length - 1 !== entities.length)
+    return <CustomTag className={className}>{fallback}</CustomTag>;
+
+  return (
+    <CustomTag className={className}>
+      {textPieces[0]}
+      {entities.map(({ text, color, url }, i) => (
+        <Fragment key={i}>
+          <a href={url} style={{ color }}>
+            {text}
+          </a>
+          {textPieces[i + 1]}
+        </Fragment>
+      ))}
+    </CustomTag>
+  );
 }
 
 export default FormatText;
